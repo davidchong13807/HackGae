d60e0bdeb8c74e45b6dbd8b68f412a7c14bea0cf
davidchong13807 <45132309+davidchong13807@users.noreply.github.com>
2018-11-17 20:34:02 -0800

    Add files via upload

--- game.js
+++ game.js
@@ -1,15 +1,15 @@
 var myGamePiece;
+var health = 100;
 var boss;
-var projectile;
 //Left, Up, Right, Down
-var keys= [];
+var keys = [];
+var projectileArr = [100];
 
 function startGame() {
     myGameArea.start();
-    myGamePiece = new component(100, 80, "red", 100, 100);
+    myGamePiece = new component(140, 110, "red", 100, 400);
     boss= new component(600, 550, "blue", 600, 0);
-    projectile = new projectileComponent (60, 60, "green", 1100, 70);
-	
+    createProjectiles();
 }
 
 var myGameArea = {
@@ -26,17 +26,124 @@ var myGameArea = {
     }
 }
 
-function updateGameArea(){
+var counter = 0;
+function updateGameArea() {
 	myGameArea.clear();
 	boss.update();
 	myGamePiece.update();
-	projectile.update();
-		
-	moveProjectile(projectile);	
-	projectileHit(projectile);	
+	
+	for(var i = 0; i < 40; i++) {
+		projectileArr[i].update();
+		moveProjectile(projectileArr[i]);
+		projectileHit(projectileArr[i]);
+	}	
 
 	checkKeys();
 	gravity();
+		
+	if(counter > 550) {
+		counter = 0;
+	}
+	
+	cycleProjectiles(counter);
+	counter++;
+	
+}
+
+function cycleProjectiles(counter) {
+	switch(counter) {
+		case 0: 
+			spawnProjectilesSet(projectileArr, 0, 8);
+			break;
+		case 50:
+			spawnProjectilesSet(projectileArr, 20, 25);
+			break;
+		case 150: 
+			spawnProjectilesSet(projectileArr, 10, 18);
+			break;
+		case 220:
+			spawnProjectilesSet(projectileArr, 30, 31);
+			break;
+		case 240:
+			spawnProjectilesSet(projectileArr, 31, 32);
+			break;
+		case 260:
+			spawnProjectilesSet(projectileArr, 32, 33);
+			break;
+		case 280:
+			spawnProjectilesSet(projectileArr, 33, 34);
+			break;
+		case 300: 
+			spawnProjectilesSet(projectileArr, 34, 35);
+			break;
+		case 320:
+			spawnProjectilesSet(projectileArr, 39, 40);
+			break;
+		case 340:
+			spawnProjectilesSet(projectileArr, 38, 39);
+			break;
+		case 360:
+			spawnProjectilesSet(projectileArr, 37, 38);
+			break;
+		case 380:
+			spawnProjectilesSet(projectileArr, 36, 37);
+			break;
+		case 380: 
+			spawnProjectilesSet(projectileArr, 35, 36);
+			break;
+		case 400: 
+			spawnProjectilesSet(projectileArr, 30, 31);
+			break;
+		case 420:
+			spawnProjectilesSet(projectileArr, 31, 32);
+			break;
+		case 440:
+			spawnProjectilesSet(projectileArr, 32, 33);
+			break;
+		case 460:
+			spawnProjectilesSet(projectileArr, 33, 34);
+			break;
+		case 480:
+			spawnProjectilesSet(projectileArr, 34, 35);
+			break;
+	}
+}
+
+function spawnProjectilesSet(arr, start, end) {
+	for(var i = start; i < end; i++) {
+		arr[i].x = 1100;
+		arr[i].y = 70;
+	}
+} 
+
+function createProjectiles() {
+	deltaTheta = 11.25 * Math.PI / 180;
+	for(var i = 0; i < 10; i++) {
+		projectileArr[i] = new projectileComponent(20, 20, 'green', -1000, -1000, fanPattern());		
+	}
+	
+	theta = 0;
+    	for(var i = 10; i < 20; i++) {
+		projectileArr[i] = new projectileComponent(20, 20, 'green', -1000, -1000, fanPattern());	
+   	}
+
+	deltaTheta = Math.PI / 10;
+	theta = 0;
+	for(var i = 20; i < 25; i++) {
+		projectileArr[i] = new projectileComponent(20, 20, 'green', -1000, -1000, fanPattern());
+	}
+
+	theta = Math.PI / 9;
+	deltaTheta = Math.PI / 15;
+	for(var i = 25; i < 30; i++) {
+		projectileArr[i] = new projectileComponent(20, 20, 'green', -1000, -1000, fanPattern());
+	}
+
+	deltaTheta = Math.PI / 20;
+	theta = 0;
+	for(var i = 30; i < 40; i++) {
+		projectileArr[i] = new projectileComponent(20, 20, 'green', -1000, -1000, fanPattern());		
+	}
 }
 
 function component(width, height, color, x, y) {
@@ -55,7 +162,7 @@ function component(width, height, color, x, y) {
 	}
 }
 
-function projectileComponent(width, height, color, x, y) {
+function projectileComponent(width, height, color, x, y, dir) {
 	this.width = width;
 	this.height = height;  
 	this.x = x;
@@ -64,11 +171,13 @@ function projectileComponent(width, height, color, x, y) {
    		ctx = myGameArea.context;
     		ctx.fillStyle = color;
    		ctx.fillRect(this.x, this.y, this.width, this.height);
+		
 	}
 	this.newPos = function() {
 		this.x = 600;
 		this.y = 225;
 	}
+	this.dir = dir;
 }
 
 window.addEventListener("keydown", function(event) {
@@ -104,13 +213,13 @@ function checkKeys() {
 var moveSpeed=5;
 
 function moveX(object, x) {
-	if(object.x > -100 && object.x < 1200) {
+	if(object.x > -500 && object.x < 1500) {
 		object.x += moveSpeed * x;
 	}
 }
 
 function moveY(object, y) {
-	if(object.y > -100 && object.y < 500) {
+	if(object.y > -500 && object.y < 1000) {
 		object.y += moveSpeed * y;
 	}
 }
@@ -162,29 +271,36 @@ function gravity() {
 }
 
 function moveProjectile(object) {
-	moveX(object, getDir(object)[0]);
-	moveY(object, getDir(object)[1]);
-	if(object.x < -50 || object.x > 1200 || object.y < 0 || object.y > 600) {
-		object.x = 1100;
-		object.y = 100;
+	if(object.x > -1000 && object.y > -1000) {
+		moveX(object, object.dir[0]);
+		moveY(object, object.dir[1]);
 	}
-}
-
-function getDir(object) {
-	var deltaX = myGamePiece.x - object.x;
-	var deltaY = myGamePiece.y - object.y;
-	var distance = Math.sqrt(Math.pow(deltaX, 2), Math.pow(deltaY, 2));
-	
-	var x = deltaX / distance;
-	var y = deltaY / distance;
-	var arr = [x, y];
-	return arr;
 }
 
 function projectileHit(object) {
-	if(object.x == myGamePiece.x || object.y == myGamePiece.y) {
-		alert('Dead');
+	if(myGamePiece.x < (object.x + object.width - 5) && 
+	   myGamePiece.x + myGamePiece.width - 15 > object.x &&
+	   myGamePiece.y < (object.y + object.height - 5) &&
+	   myGamePiece.y + myGamePiece.height - 15 > object.y) {
+		health -= 34;
+		object.x = -1000;
+		object.y = -1000;
+	}
+	
+	if(health <= 0) {
+		alert('dead');
 	}
 }
+
+var theta = 0;
+var deltaTheta;
+function fanPattern() {
+	var dir = [-1 * Math.cos(theta), Math.sin(theta)];
+	theta += deltaTheta;
+	if(theta == (Math.PI / 2)) {
+		theta = 0;
+	}
+	return dir;
+} 
 
 
