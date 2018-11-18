a0d64ed7caaaccdf0a8402a13b3f11b3d7186606
davidchong13807 <45132309+davidchong13807@users.noreply.github.com>
2018-11-17 16:46:30 -0800

    Add files via upload

--- game.js
+++ game.js
@@ -1,22 +1,15 @@
-var myGamePiece; 
-
+var myGamePiece;
+var boss;
+var projectile;
 //Left, Up, Right, Down
 var keys= [];
 
-window.addEventListener("keydown", function(event){
-	keys[event.keyCode] = true;
-	console.log(event.keyCode);
-}, true);
-
-window.addEventListener("keyup", function(event) {
-	keys[event.keyCode] = false;
-}, true);
-
-var moveSpeed=5;
-
 function startGame() {
     myGameArea.start();
-    myGamePiece = new component(100, 80, "1bun.png", 100, 100, "image")
+    myGamePiece = new component(100, 80, "red", 100, 100);
+    boss= new component(600, 550, "blue", 600, 0);
+    projectile = new projectileComponent (60, 60, "green", 1100, 70);
+	
 }
 
 var myGameArea = {
@@ -28,109 +21,115 @@ var myGameArea = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 	this.interval = setInterval(updateGameArea, 20);
     },
-	clear: function(){
-		this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
-
-	}
+    clear: function() {
+	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
 }
 
-
-function component(width, height, color, x, y, type) {
- 	this.type = type;
-	
-	if (type == "image") {
-		
-		this.image = new Image();
-		
-		this.image.src = color;
-	}
-	
-	this.width = width;
-    
-	this.height = height;  
-	
-	this.x=x;
-	
-	this.y=y;
-	
-	
-	this.update = function(){
-   	 
-		ctx = myGameArea.context;
-    	
-		if (type == "image") {
-	
-			ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-	} 
-		else {
-	ctx.fillStyle = color;
-	cts.fillRect(this.x, this.y, this.width, this.height);
-	}
-	
-}
-
-
-  /* this.width = width;
-    this.height = height;  
-	this.x=x;
-	this.y=y;
-	this.update = function(){
-   	 ctx = myGameArea.context;
-    	ctx.fillStyle = color;
-   	 ctx.fillRect(this.x, this.y, this.width, this.height);
-	}
-	this.newPos = function(){
-		this.x = 600;
-		this.y = 225;
-	}*/
-}
 function updateGameArea(){
 	myGameArea.clear();
-	//myGamePiece.newPos();
+	boss.update();
 	myGamePiece.update();
+	projectile.update();
+		
+	moveProjectile(projectile);	
+	projectileHit(projectile);	
+
 	checkKeys();
 	gravity();
 }
 
-function moveUp(){
-	if(myGamePiece.y==0){
-		myGamePiece.y+=0;
+function component(width, height, color, x, y) {
+  	this.width = width;
+	this.height = height;  
+	this.x = x;
+	this.y = y;
+	this.update = function() {
+   		ctx = myGameArea.context;
+    		ctx.fillStyle = color;
+   		ctx.fillRect(this.x, this.y, this.width, this.height);
 	}
-	else{
-		myGamePiece.y -=moveSpeed;
-	}
-	
-}
-function moveDown(){
-	
-	if(myGamePiece.y==550){
-		myGamePiece.y+=0;
-	}
-	else{
-		myGamePiece.y +=moveSpeed;
+	this.newPos = function() {
+		this.x = 600;
+		this.y = 225;
 	}
 }
-function moveLeft(){
-	
-	if(myGamePiece.x==0){
-		myGamePiece.x-=0;
+
+function projectileComponent(width, height, color, x, y) {
+	this.width = width;
+	this.height = height;  
+	this.x = x;
+	this.y = y;
+	this.update = function() {
+   		ctx = myGameArea.context;
+    		ctx.fillStyle = color;
+   		ctx.fillRect(this.x, this.y, this.width, this.height);
 	}
-	else{
+	this.newPos = function() {
+		this.x = 600;
+		this.y = 225;
+	}
+}
+
+window.addEventListener("keydown", function(event) {
+	keys[event.keyCode] = true;
+	console.log(event.keyCode);
+}, true);
+
+window.addEventListener("keyup", function(event) {
+	keys[event.keyCode] = false;
+}, true);
+
+function checkKeys() {
+	if(keys[37]) {
+		movePlayerLeft();
+	}
+	if(keys[39]) {
+		movePlayerRight();
+	}
+	if(keys[17]) {
+		moveSpeed = 15;
+	}
+	else {
+		moveSpeed = 5;
+	}
+	if(keys[38]) {
+		hop();
+	}	
+	else {
+		fall();
+	}	
+}		
+
+var moveSpeed=5;
+
+function moveX(object, x) {
+	if(object.x > -100 && object.x < 1200) {
+		object.x += moveSpeed * x;
+	}
+}
+
+function moveY(object, y) {
+	if(object.y > -100 && object.y < 500) {
+		object.y += moveSpeed * y;
+	}
+}
+
+function movePlayerLeft() {
+	if(myGamePiece.x > 0) {
 		myGamePiece.x -= moveSpeed;
 	}	
 }
-function moveRight(){
-	if(myGamePiece.x>=1200){
-		myGamePiece.x-=0;
-	}
-	else{
+
+function movePlayerRight() {
+	if(myGamePiece.x < 1100) {
 		myGamePiece.x += moveSpeed;
 	}
 }
 
 var jumpForce = 20;
 var hasJumped = false;
-function hop(){
+function hop() {
 	if(!hasJumped) {
 		myGamePiece.y -= jumpForce;
 	}
@@ -143,27 +142,6 @@ function fall() {
 	else {
 		hasJumped = true;
 		myGamePiece.y -= jumpForce;
-	}
-}
-
-function checkKeys(){	
-	if (keys[37]){
-	moveLeft();
-	}
-	if (keys[39]){
-	moveRight();
-	}
-	if (keys[17]){
-	moveSpeed = 15;
-	}
-	else{
-	moveSpeed = 5;
-	}
-	if (keys[38]){
-	hop();
-	}	
-	if(!keys[38]) {
-	fall();
 	}
 }
 
@@ -182,3 +160,31 @@ function gravity() {
 	//console.log(g);
 	//console.log(myGamePiece.y);
 }
+
+function moveProjectile(object) {
+	moveX(object, getDir(object)[0]);
+	moveY(object, getDir(object)[1]);
+	if(object.x < -50 || object.x > 1200 || object.y < 0 || object.y > 600) {
+		object.x = 1100;
+		object.y = 100;
+	}
+}
+
+function getDir(object) {
+	var deltaX = myGamePiece.x - object.x;
+	var deltaY = myGamePiece.y - object.y;
+	var distance = Math.sqrt(Math.pow(deltaX, 2), Math.pow(deltaY, 2));
+	
+	var x = deltaX / distance;
+	var y = deltaY / distance;
+	var arr = [x, y];
+	return arr;
+}
+
+function projectileHit(object) {
+	if(object.x == myGamePiece.x || object.y == myGamePiece.y) {
+		alert('Dead');
+	}
+}
+
+
